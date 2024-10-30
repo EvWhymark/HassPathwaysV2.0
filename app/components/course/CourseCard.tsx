6cdd0fc61601693f0d9663feeb1016e75d11a238
Giancarlo <giancarlo@gcmart.net>
2024-10-30 11:55:48 -0400

    Extract Header from Course Card Flex Box, Fixed missing prereq and UIA bugs

--- app/components/course/CourseCard.tsx
+++ app/components/course/CourseCard.tsx
@@ -10,6 +10,7 @@ const offeredSemestersChecker = ( term: IOfferedSchema | undefined) => {
   if (term.years[term.years.length - 1].fall) offeredSemesters.push("Fall");
   if (term.years[term.years.length - 1].spring) offeredSemesters.push("Spring");
   if (term.years[term.years.length - 1].summer) offeredSemesters.push("Summer");
+  if (term.uia) offeredSemesters.push("Upon Instructor Availability");
   return offeredSemesters;
 };
 
@@ -32,9 +33,9 @@ const CourseCard = ({
   const offeredSemesters = offeredSemestersChecker(term);
   return (
     <section className="course-card">
-      <div className="flex flex-col fold:flex-row justify-between items-start">
-        <div className="flex flex-col">
-          <header className="course-title">
+      <header className="course-title">
+        <div className="flex flex-row items-center">
+          <div>
             <Link
               href={`/courses/${subject + '-' + courseCode}`}
               className="text-md font-semibold break-normal"
@@ -42,8 +43,16 @@ const CourseCard = ({
               {title}
             </Link>
             <p className="text-sm text-utility-gray-600">{subject + '-' + courseCode}</p>
-          </header>
-          <div className="flex gap-x-1 flex-wrap mt-2">
+          </div>
+          <div className="flex-grow"></div>
+          <div className="flex-end">
+            <CourseCardDropDown title={title} courseCode={courseCode} status={status} />
+          </div>
+        </div>
+      </header>
+      <div className="flex flex-col fold:flex-row justify-between items-start">
+        <div className="flex flex-col">
+          <div className="flex gap-x-1 flex-wrap">
             {attributes && attributes.CI && (
               <p className="tag tag-primary">
                 Communication Intensive
@@ -55,28 +64,31 @@ const CourseCard = ({
               </p>
             )}
           </div>
-          {offeredSemesters.length > 0 && (
-            <div className="mt-2">
-              <h4 className="text-sm font-semibold">Offered:</h4>
-              <ul className="list-disc ml-4">
-                {offeredSemesters.map((semester) => (
-                  <li key={semester} className="text-sm text-utility-gray-600">
-                    {semester}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-          {prereqs && prereqs.courses.length > 0 && (
-            <div className="mt-2">
-              <h4 className="text-sm font-semibold">Prerequisites:</h4>
-              <ul className="text-sm text-utility-gray-600">
-                {prereqs.raw_precoreqs}
-              </ul>
-            </div>
-          )}
+          <div className="flex flex-wrap">
+            {offeredSemesters.length > 0 && (
+              <div className="mt-2">
+                <h4 className="text-sm font-semibold">Offered:</h4>
+                <ul className="list-disc ml-4">
+                  {offeredSemesters.map((semester) => (
+                    <li key={semester} className="text-sm text-utility-gray-600">
+                      {semester}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-wrap">
+            {prereqs && prereqs.raw_precoreqs != "" && (
+              <div className="mt-2">
+                <h4 className="text-sm font-semibold">Prerequisites:</h4>
+                <ul className="text-sm text-utility-gray-600">
+                  {prereqs.raw_precoreqs}
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
-        <CourseCardDropDown title={title} courseCode={courseCode} status={status} />
       </div>
     </section>
   );
